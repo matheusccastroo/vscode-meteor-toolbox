58d3886ed43043a878ae11c4cc1e7870859e17fd
Matheus Castro <camargodecastromatheus@gmail.com>
2022-10-16 00:58:47 -0300

    Organizing and using JS instead of TS for server

--- extension.js
+++ extension.js
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
 const {
     addDebugAndRunOptions,
@@ -13,6 +11,10 @@ const {
     addImportedPackagesToJsConfig,
     addCustomPackageOptionsToJsConfig,
 } = require("./src/packages-config");
+const {
+    connectToLanguageServer,
+    stopServer,
+} = require("./src/connect-to-server");
 
 const createOrUpdateJsConfigFile = () => {
     generateBaseJsConfig();
@@ -28,55 +30,11 @@ const disposeWatchers = () =>
         (watcher) => watcher?.dispose?.()
     );
 
-const vscode_1 = require("vscode");
-const path = require("path");
-const node_1 = require("vscode-languageclient/node");
-
-let client;
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
     console.log("Starting Meteor Toolbox extension...");
-    // The server is implemented in node
-    const serverModule = context.asAbsolutePath(
-        path.join("spacebars", "server", "out", "server.js")
-    );
-    // The debug options for the server
-    // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-    const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
-    // If the extension is launched in debug mode then the debug server options are used
-    // Otherwise the run options are used
-    const serverOptions = {
-        run: { module: serverModule, transport: node_1.TransportKind.ipc },
-        debug: {
-            module: serverModule,
-            transport: node_1.TransportKind.ipc,
-            options: debugOptions,
-        },
-    };
-    // Options to control the language client
-    const clientOptions = {
-        // Register the server for plain text documents
-        documentSelector: [
-            { scheme: "file", language: "html" },
-            { scheme: "file", language: "javascript" },
-        ],
-        synchronize: {
-            // Notify the server about file changes to '.clientrc files contained in the workspace
-            fileEvents:
-                vscode_1.workspace.createFileSystemWatcher("**/.clientrc"),
-        },
-    };
-    // Create the language client and start the client.
-    client = new node_1.LanguageClient(
-        "languageServerExample",
-        "Language Server Example",
-        serverOptions,
-        clientOptions
-    );
-    // Start the client. This will also launch the server
-    client.start();
 
     addDebugAndRunOptions();
     createOrUpdateJsConfigFile();
@@ -167,15 +125,15 @@ function activate(context) {
         clearMeteorBuildCacheDisposable,
         regenerateLaunchJsonDisposable
     );
+
+    console.log("Connecting to language server...");
+    connectToLanguageServer(context.asAbsolutePath);
 }
 
 // this method is called when your extension is deactivated
 function deactivate() {
     disposeWatchers();
-    if (!client) {
-        return undefined;
-    }
-    return client.stop();
+    stopServer();
 }
 
 module.exports = {
